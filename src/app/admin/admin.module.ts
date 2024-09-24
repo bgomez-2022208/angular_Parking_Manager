1b29fa54575f79554d12ee2764a7c50f382ca9a4
H3721N <herlingomezescobar@outlook.com>
2024-09-23 19:33:47 -0600

    se crea el modulo de admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchComponent } from './search/search.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ProfilesComponent } from './profiles/profiles.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchComponent,
+    ProfilesComponent
+  ],
   imports: [
     CommonModule
   ]
