3ebd646259c4fda3319919a041187ede40aff461
H3721N <herlingomezescobar@outlook.com>
2024-09-23 19:36:17 -0600

    se ajusta el module app para mostrar las rutas hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AuditoriaComponent } from './auditoria/auditoria.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,7 +21,6 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
-    AuditoriaComponent,
     MatNativeDateModule,
     ReactiveFormsModule
   ],
