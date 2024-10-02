c92ae6359ac12ab2f0b427ed102c2eac4b1112d3
H3721N <herlingomezescobar@outlook.com>
2024-10-02 14:09:43 -0600

    Se ajusta el search por fecha

--- src/app/admin/auditoria/auditoria.component.ts
+++ src/app/admin/auditoria/auditoria.component.ts
@@ -1,57 +1,51 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
-import {MatSort, MatSortModule} from '@angular/material/sort';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatSelectChange } from '@angular/material/select';
-import { MatNativeDateModule } from '@angular/material/core';
 import { FormControl } from '@angular/forms';
-
-
+import { DatePipe } from '@angular/common';
 
 export interface AuditData {
   id: string;
   entity: string;
   change: string;
-  date: string;
+  date: Date;
+  description: string;
 }
 
 @Component({
   selector: 'app-auditoria',
   templateUrl: './auditoria.component.html',
   styleUrls: ['./auditoria.component.scss'],
-
-  providers: [MatDatepickerModule,MatButtonModule],
+  providers: [DatePipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuditoriaComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'entity', 'change', 'date'];
+  displayedColumns: string[] = ['id', 'entity', 'change', 'date','description'];
   dataSource: MatTableDataSource<AuditData>;
-  selected= 'valor';
+  selected: string = ''; 
   private readonly _currentYear = new Date().getFullYear();
   readonly minDate = new Date(this._currentYear - 20, 0, 1);
   readonly maxDate = new Date();
 
+  selectedDateControl = new FormControl();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() {
+  constructor(private datePipe: DatePipe) {
     const audit: AuditData[] = [
-      { id: '1', entity: 'User', change: 'Updated password', date: '2023/09/01' },
-      { id: '2', entity: 'User', change: 'Created account', date: '2023/09/02' },
-      { id: '3', entity: 'Order', change: 'Deleted order', date: '2023/09/03' },
-      { id: '4', entity: 'Product', change: 'Updated price', date: '2023/09/04' }
+      { id: '1', entity: 'User', change: 'Updated password', date: new Date('2024-09-01'),description: 'Successfully loaded user by email: juangalicia@gmail.com'},
+      { id: '2', entity: 'User', change: 'Created account', date: new Date('2023-09-02'),description: 'Successfully loaded user by email: juangalicia@gmail.com'},
+      { id: '3', entity: 'Profile', change: 'Deleted order', date: new Date('2023-09-03'),description: 'Successfully loaded user by email: juangalicia@gmail.com'},
+      { id: '4', entity: 'Product', change: 'Updated price', date: new Date('2023-09-04'),description: 'Successfully loaded user by email: juangalicia@gmail.com'}
     ];
 
     this.dataSource = new MatTableDataSource(audit);
-
+    this.selectedDateControl.valueChanges.subscribe(value => {
+      this.applyFilter(value);
+    });
   }
 
   ngAfterViewInit() {
@@ -63,38 +57,48 @@ export class AuditoriaComponent implements AfterViewInit {
     let filterValue = '';
 
     if (event instanceof Date) {
-      const selectedDate = event;
-      const day = String(selectedDate.getDate()).padStart(2, '0');
-      const month = String(selectedDate.getMonth() + 1).padStart(2, '0');
-      const year = selectedDate.getFullYear();
-
-      filterValue = `${year}-${month}-${day}`;
-    }
-    else if (event instanceof MatSelectChange) {
+      filterValue = this.datePipe.transform(event, 'yyyy-MM-dd') || '';
+    } else if (event instanceof MatSelectChange) {
       filterValue = event.value;
-    }
-    else if (event.target && (event.target as HTMLInputElement).value !== undefined) {
+    } else if (event.target && (event.target as HTMLInputElement).value !== undefined) {
       filterValue = (event.target as HTMLInputElement).value;
     }
 
+    console.log('Filter Value:', filterValue);
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
   }
+
+  applyDateFilter(selectedDate: Date) {
+    if (selectedDate) {
+      const formattedDate = this.datePipe.transform(selectedDate, 'yyyy-MM-dd');
+      this.dataSource.filter = formattedDate ? formattedDate.trim().toLowerCase() : '';
+    }
+  
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+
   ngOnInit() {
     this.dataSource.filterPredicate = (data: AuditData, filter: string) => {
       const transformedFilter = filter.trim().toLowerCase();
+      const dateString = this.formatDate(data.date);
 
       const matchFilter =
         data.id.toLowerCase().includes(transformedFilter) ||
         data.entity.toLowerCase().includes(transformedFilter) ||
         data.change.toLowerCase().includes(transformedFilter) ||
-        data.date.includes(transformedFilter);
+        (dateString ? dateString.includes(transformedFilter) : false);
 
       return matchFilter;
     };
   }
-
+  formatDate(date: Date | string): string | null {
+    return this.datePipe.transform(date, 'yyyy-MM-dd');
+  }
 }
