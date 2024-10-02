a4c5e90a82ed64706688e00e26f2ba2fcc9ba594
bgomez-2022208 <bgomez-2022208@kinal.edu.gt>
2024-10-02 14:06:39 -0600

    Modificacion al componente reutilizable

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -17,12 +17,20 @@ export class UsersComponent implements OnInit {
   currentPage: number = 1;
 
   defaultUsers: User[] = [
-    { email: 'user1@example.com', name: 'User One'},
-    { email: 'user2@example.com', name: 'User Two',},
-    { email: 'user3@example.com', name: 'User Three'},
-    { email: 'user4@example.com', name: 'User Four',},
-    { email: 'user5@example.com', name: 'User Five',},
+    { email: 'user1@example.com', name: 'User One' },
+    { email: 'user2@example.com', name: 'User Two' },
+    { email: 'user3@example.com', name: 'User Three' },
+    { email: 'user4@example.com', name: 'User Four' },
+    { email: 'user5@example.com', name: 'User Five' },
+    { email: 'user6@example.com', name: 'User Six' },
+    { email: 'user7@example.com', name: 'User Seven' },
+    { email: 'user8@example.com', name: 'User Eight' },
+    { email: 'user9@example.com', name: 'User Nine' },
+    { email: 'user10@example.com', name: 'User Ten' },
+    { email: 'user11@example.com', name: 'User Eleven' },
+    { email: 'user12@example.com', name: 'User Twelve' }
   ];
+
 
   constructor(private apiUserService: ApiUserService) {}
 
