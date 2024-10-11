8eebd9b98265d3b4c8977c49f8b7604121f4a592
H3721N <herlingomezescobar@outlook.com>
2024-10-11 08:46:27 -0600

    Se muestra el componente de tabla para tarifas

--- src/app/admin/maintenance/components/fare-table/fare-table.component.ts
+++ src/app/admin/maintenance/components/fare-table/fare-table.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FareService, FareData} from '../../../services/fare.service';
+import { MatDialog } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fare-table',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./fare-table.component.scss']
 })
 export class FareTableComponent {
+  @Input() fare: FareData[] = [];
+  @Input() itemsPerPage: number = 0;
+  @Output() pageChange = new EventEmitter<number>();
+  @Input() totalPages: number = 0;
+  searchQuery: string = "";
+
+  constructor(private fareService: FareService,
+              private dialog: MatDialog,
+              private translate: TranslateService,
+              private router: Router) {}
+
+  @Input() currentPage: number = 1;
+  @Input() profiles!: any[];
+  @Input() itemsPerPageOptions!: number[];
+  @Output() profileSelected = new EventEmitter<number>();
+  @Output() profileDeleted = new EventEmitter<number>();
+
+  ngOnInit(): void {
+    this.fareService.getAllFare().subscribe(
+      (data: any) => {
+        console.log('Datos recibidos:', data);
+        this.fare = data.users;
+        console.log('hello, this is: ', this.fare);
+        this.totalPages = data.totalPages;
+      },
+      (error: any) => {
+        console.error('Error al cargar tarifas:', error);
+      }
+    );
+  }
+
+
+
 
 }
