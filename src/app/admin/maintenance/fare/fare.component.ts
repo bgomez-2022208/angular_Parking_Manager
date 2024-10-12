ace240d3103265620fcecb9557c7e2ee4f7eb4a3
H3721N <herlingomezescobar@outlook.com>
2024-10-12 10:16:23 -0600

    Se realiza el metodo de update a los registros

--- src/app/admin/maintenance/fare/fare.component.ts
+++ src/app/admin/maintenance/fare/fare.component.ts
@@ -3,6 +3,8 @@ import { MatTableDataSource } from '@angular/material/table';
 import { FareData, FareService } from '../../services/fare.service';
 import { DatePipe } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-fare',
@@ -12,6 +14,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FareComponent {
   dataSource: MatTableDataSource<FareData>;
   fareForm: FormGroup;
+  isEditing = false;
+  private fareId: number = 0;
 
   constructor(private datePipe: DatePipe, private fareService: FareService, private fb: FormBuilder) {
     const fare: FareData[] = [];
@@ -22,30 +26,64 @@ export class FareComponent {
       startTime: ['', Validators.required],
       price: ['', Validators.required],
       endTime: ['', Validators.required],
+      status: [false,Validators.required]
     });
   }
 
-  onSubmit(): void {
-    console.log('submit');
-    console.log('Submitted fare data:', this.fareForm.value);
+  isEdit: boolean = false;
+  selectedFareId: number | null = null;
+
+  onSubmit() {
     if (this.fareForm.valid) {
-      const fareData: FareData = this.fareForm.value;
-      this.fareService.createFare(fareData).subscribe({
-        next: () => {
-          this.fareForm.reset();
-        },
-        error: (err) => {
-          console.error('Error al agregar la tarifa', err);
-        }
-      });
-    } else {
-      console.log('Formulario no válido');
+      console.log('isEditing:', this.isEditing);
+      console.log('fareId:', this.fareId);
+      const fareData: FareData = {
+        fareId: this.isEditing ? this.fareId : 0,
+        name: this.fareForm.value.name,
+        startTime: this.fareForm.value.startTime,
+        endTime: this.fareForm.value.endTime,
+        price: Number(this.fareForm.value.price),
+        status: this.fareForm.value.status
+      };
+
+      if (this.isEditing) {
+        this.fareService.updateFare(this.fareId, fareData).subscribe(
+          () => {
+            Swal.fire('Success', 'Fare updated successfully', 'success');
+            this.resetForm();
+          },
+          (error) => {
+            Swal.fire('Error', 'Failed to update fare', 'error');
+            console.error('Error updating fare:', error);
+          }
+        );
+      } else {
+        this.fareService.createFare(fareData).subscribe(
+          () => {
+            Swal.fire('Success', 'Fare created successfully', 'success');
+            this.resetForm();
+          },
+          (error) => {
+            Swal.fire('Error', 'Failed to create fare', 'error');
+            console.error('Error creating fare:', error);
+          }
+        );
+      }
     }
   }
 
+  resetForm(): void {
+    this.fareForm.reset();
+    this.fareId = 0;
+    this.isEditing = false;
+  }
+
+
+
   onFareSelected(fare: FareData): void {
-    // Asegúrate de que las propiedades coincidan con FareData
     console.log('Fare data received in FareComponent:', fare);
+    this.isEditing = true;  // Cambia el estado para indicar que estamos editando
+    this.fareId = fare.fareId;
     this.fareForm.patchValue({
       name: fare.name,
       startTime: fare.startTime,
