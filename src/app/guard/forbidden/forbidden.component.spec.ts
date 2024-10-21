a5f1a00a18016834989b46b145a3d83b29cb0d63
bgomez-2022208 <bgomez-2022208@kinal.edu.gt>
2024-10-21 14:00:36 -0600

    Se arreglaron los errores de karma con login y register y recuperar por email

--- src/app/guard/forbidden/forbidden.component.spec.ts
+++ src/app/guard/forbidden/forbidden.component.spec.ts
@@ -1,6 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ForbiddenComponent } from './forbidden.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'translate' })
+export class MockTranslatePipe implements PipeTransform {
+  transform(value: string): string {
+    return value;
+  }
+}
+
+class TranslateServiceMock {
+  get(key: string) {
+    return of(key);
+  }
+
+  setDefaultLang(lang: string) {
+  }
+
+  use(lang: string) {
+  }
+}
 
 describe('ForbiddenComponent', () => {
   let component: ForbiddenComponent;
@@ -8,8 +29,15 @@ describe('ForbiddenComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ForbiddenComponent]
+      imports: [
+        TranslateModule.forRoot()
+      ],
+      declarations: [ForbiddenComponent, MockTranslatePipe],
+      providers: [
+        { provide: TranslateService, useClass: TranslateServiceMock }
+      ]
     });
+
     fixture = TestBed.createComponent(ForbiddenComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/user/security/forgot-password-email/forgot-password-email.component.spec.ts
+++ src/app/user/security/forgot-password-email/forgot-password-email.component.spec.ts
@@ -1,21 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { ForgotPasswordEmailComponent } from './forgot-password-email.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field'; // Importa MatFormFieldModule
+import { MatInputModule } from '@angular/material/input'; // Si usas matInput, también debes importar este módulo
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('ForgotPasswordEmailComponent', () => {
-  let component: ForgotPasswordEmailComponent;
-  let fixture: ComponentFixture<ForgotPasswordEmailComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ForgotPasswordEmailComponent]
-    });
-    fixture = TestBed.createComponent(ForgotPasswordEmailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ForgotPasswordEmailComponent],
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        TranslateService
+      ]
+    }).compileComponents();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(ForgotPasswordEmailComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
