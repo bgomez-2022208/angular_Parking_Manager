1b0cd4dea766fc96e9498eac4889b61cead1239b
H3721N <herlingomezescobar@outlook.com>
2024-10-21 15:11:38 -0600

    Se recupera cambio de una correccion

--- src/app/user/security/forgot-password-two/forgot-password-two.component.spec.ts
+++ src/app/user/security/forgot-password-two/forgot-password-two.component.spec.ts
@@ -1,14 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ForgotPasswordTwoComponent } from './forgot-password-two.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ForgotPasswordTwoComponent', () => {
   let component: ForgotPasswordTwoComponent;
   let fixture: ComponentFixture<ForgotPasswordTwoComponent>;
 
+  const mockActivatedRoute = {
+    snapshot: {
+      paramMap: {
+        get: (key: string) => {
+          return null;
+        }
+      }
+    }
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ForgotPasswordTwoComponent]
+      declarations: [ForgotPasswordTwoComponent],
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatMenuModule,
+        MatTableModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute } // Proveedor para ActivatedRoute
+      ]
     });
     fixture = TestBed.createComponent(ForgotPasswordTwoComponent);
     component = fixture.componentInstance;
