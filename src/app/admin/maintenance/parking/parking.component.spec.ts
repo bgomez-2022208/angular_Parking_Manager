e04985bedefff44ffb51aa01f5a2dab13244e358
H3721N <herlingomezescobar@outlook.com>
2024-10-21 14:54:54 -0600

    correccion con el karma

--- src/app/admin/maintenance/parking/parking.component.spec.ts
+++ src/app/admin/maintenance/parking/parking.component.spec.ts
@@ -1,6 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ParkingComponent } from './parking.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NavbaruserComponent } from '../components/navbaruser/navbaruser.component';
 
 describe('ParkingComponent', () => {
   let component: ParkingComponent;
@@ -8,7 +23,23 @@ describe('ParkingComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ParkingComponent]
+      declarations: [ParkingComponent,NavbaruserComponent],
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatMenuModule,
+        MatTableModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule
+      ],
     });
     fixture = TestBed.createComponent(ParkingComponent);
     component = fixture.componentInstance;
